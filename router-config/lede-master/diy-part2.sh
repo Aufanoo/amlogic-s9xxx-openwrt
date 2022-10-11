797379681d31288b0c6dea1a08c98d7e7ce54c64
Aufanoo <112615679+Aufanoo@users.noreply.github.com>
2022-10-12 03:29:05 +0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -29,6 +29,17 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # ------------------------------- Main source ends -------------------------------
 
+# ------------------------------- External source start ---------------------------
+
+## luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
+
+## luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
+pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
+
+#-------------------------------- External source ends --------------------------
+
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
